5338e01b73fc82058ead7997aa2ec1b42c030a05
JPetta <jovipetraratulangi@gmail.com>
2019-04-10 14:11:24 +0700

    first commit

---
+++ DemoWhileLoops/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DemoWhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter your name : ");
+                var name = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(name))
+                {
+                    Console.WriteLine("@echo : " + name);
+                    continue;
+                }
+
+                break;
+            }
+        }
+    }
+}
